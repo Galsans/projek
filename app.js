32bb7f4a488297c13845cce561c0425f95f1cb39
Galsans <glhsmk21357@gmail.com>
2023-02-22 17:54:09 +0700

    new codingan

--- app.js
+++ app.js
@@ -13,3 +13,19 @@ bgSidebar.addEventListener('click', function() {
   sidebar.classList.remove('change')
   burger.classList.remove('change')
 })
+
+
+// NAVBAR
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header").classList.remove('show');
+  }
+  else { document.getElementById("header").classList.add('show');
+  }
+  prevScrollpos = currentScrollPos;
+}
+//]]>
+
+
